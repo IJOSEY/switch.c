e7db944475a27236ad6ed6b82bf2d50adcaf8344
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-25 14:26:07 +0000

    corrigindo erros

--- diadasemanaif.c
+++ diadasemanaif.c
@@ -5,22 +5,22 @@ int main (){
     printf("Digite um número de 1 à 7 para verificar qual dia é:\n");
     scanf("%d",&dia);
 
-    if(dia = 1){
-        printf("Domingo");
-    } else if ( dia = 2){
-        printf("Segunda-feira");
-    }else if ( dia = 3){
-        printf("Terça-feira");
-    }else if (dia = 4){
-        printf("Quarta-feira");
-    }else if (dia = 5){
-        printf("Quinta-feira");
-    } else if ( dia = 6){
-        printf("Sexta-feira");
-    } else if (dia = 7){
-        printf("Sábado");
+    if(dia == 1){
+        printf("Domingo.\n");
+    } else if ( dia == 2){
+        printf("Segunda-feira.\n");
+    }else if ( dia == 3){
+        printf("Terça-feira.\n");
+    }else if (dia == 4){
+        printf("Quarta-feira.\n");
+    }else if (dia == 5){
+        printf("Quinta-feira.\n");
+    } else if ( dia == 6){
+        printf("Sexta-feira.\n");
+    } else if (dia == 7){
+        printf("Sábado.\n");
     }else {
-        printf("Dia invalido");
+        printf("Dia invalido.\n");
     }
     return 0;
      
