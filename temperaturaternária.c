479ec3c3fdab31ba04c46cf7264ee377807d059b
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-28 15:44:34 +0000

    temperatura verificada com sucesso!

--- temperaturaternária.c
+++ temperaturaternária.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int main(){
+    float temperatura;
+    float resultadotemp;
+
+    printf("Digite a temperatura: ");
+    scanf("%f",&temperatura);
+
+    resultadotemp = temperatura >= 30.0 ? 1: 0;
+
+    if (resultadotemp == 1){
+        printf("Está calor!!\n");
+    } else {
+        printf("O clima está esfriando!\n");
+    }
+    return 0;
+}
