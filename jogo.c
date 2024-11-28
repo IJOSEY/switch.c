4fcc079435e6bdea078a1cc7ddbf71e17e6a44a9
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-28 14:36:05 +0000

    switch para imprimir as escolhas

--- jogo.c
+++ jogo.c
@@ -4,5 +4,17 @@
 int main(){
     int escolhaj, escolhac;
     srand(time(0));
+    printf("****JOKENPÔ****\n\n");
+    printf("Escolha ua das opções:\n");
+    printf("1.Pedra.\n");
+    printf("2.Papel.\n");
+    printf("3.Tesoura.\n");
+    printf("Escolha: ");
+    scanf("%d",&escolhaj);
+    escolhac = rand() % 3 +1;
+    switch(escolhaj){
+        case 1:
+        printf("")
+    }
     return 0;
 }
