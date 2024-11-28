7b0b71c6d8f7ca97783caedc725a3680c9fdb092
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-28 15:10:36 +0000

    jogo testado e funcionando!!

--- jogo.c
+++ jogo.c
@@ -14,7 +14,39 @@ int main(){
     escolhac = rand() % 3 +1;
     switch(escolhaj){
         case 1:
-        printf("")
+        printf("Jogador: Pedra-\n");
+        break;
+        case 2:
+        printf("Jogador: Papel-\n");
+        break;
+        case 3:
+        printf("Jogador: Tesoura-\n");
+        break;
+        default:
+        printf("Opção invalida.\n");
+        break;
+    }
+    switch(escolhac){
+        case 1:
+        printf("Computador: Pedra-\n");
+        break;
+        case 2:
+        printf("Computador: Papel-\n");
+        break;
+        case 3:
+        printf("Computador: Tesoura-\n");
+        break;
+        
+    }
+    if (escolhaj == escolhac){
+        printf("EMPATE!\n");    
+    }else if ((escolhac == 1) && (escolhaj == 3) || 
+              (escolhac == 2) && (escolhaj == 1) ||
+              (escolhac == 3) && (escolhaj == 2)){
+        
+        printf("O Computador venceu!\n");
+    }else {
+        printf("### Jogador venceu!!###\n");
     }
     return 0;
 }
